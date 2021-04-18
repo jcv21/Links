f5d2279a723a6c4d2c89607f5f1b1e9ac7568cca
Justin Carl Velarde <justincarl.jcv@gmail.com>
2021-04-18 13:54:58 +0800

    Update Routes

--- links/routes/web.php
+++ links/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+// Register the routes
+Auth::routes();
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +18,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $links = \App\Models\Link::all();
+
+    return view('welcome', ['links', $links]);
 });
 
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
